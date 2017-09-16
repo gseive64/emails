54f9f3a47fd1dcf274862bd90ba5350e578f70bf
Geoffroy Seive <geoffroyseive@gmail.com>
2017-09-16 18:25:31 -0500

    Completed read/unread count per week

--- PythonToMongo/PythonToMongo.py
+++ PythonToMongo/PythonToMongo.py
@@ -1,1 +1,17 @@
 
+# first program with MongoDB
+from pymongo import MongoClient
+import pprint
+client = MongoClient()
+db = client.myEmails
+emails = db.myEmailsTest5
+pprint.pprint(emails.find_one())
+#for email in emails.find():
+#    pprint.pprint(email)
+pipeline = [
+    {"$project": {"_id": 0, "unread": 1, "date":1, "year": {"$year": "$date"},  "week": {"$week": "$date"} }},	
+    {"$group": {"_id": {"week":"$week", "unread": "$unread"}, "count": {"$sum": 1}}}
+    ]
+read_unread_count = list(emails.aggregate(pipeline))
+
+pprint.pprint(read_unread_count)
